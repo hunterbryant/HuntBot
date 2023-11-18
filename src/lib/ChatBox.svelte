e07c43b655b0c551e632e63020554a82d90a3f8a
Hunter Bryant <hunter.bryant2016@gmail.com>
2023-11-18 14:52:40 -0700

    Added Chatbox component w/ responsive width

---
+++ src/lib/ChatBox.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<div
+	class="absolute bottom-2 left-2 right-2 h-96 w-[calc(full-4rem)] rounded-3xl bg-white sm:left-auto sm:w-[calc(27.5rem)]"
+></div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import '../app.css';
 	import studiobg from '$lib/assets/studiobg.webp';
+	import ChatBox from '$lib/ChatBox.svelte';
 </script>
 
 <div
-	class="h-full overflow-hidden rounded-none bg-slate-300 bg-[url('lib/assets/studiobg.webp')] bg-cover bg-center bg-no-repeat sm:rounded-[2rem]"
+	class="relative h-full overflow-hidden rounded-none bg-slate-300 bg-[url('lib/assets/studiobg.webp')] bg-cover bg-center bg-no-repeat sm:rounded-[2rem]"
 >
 	<slot />
+	<ChatBox />
 </div>
