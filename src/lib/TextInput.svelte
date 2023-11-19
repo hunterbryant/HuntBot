3c8995d8f3f33fa22afd022c93f57b2558f694eb
Hunter Bryant <hunter.bryant2016@gmail.com>
2023-11-19 16:13:28 -0700

    Added event within textbox to submit & clear msg

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -3,12 +3,21 @@
 	import arrowup from './assets/arrow-up.svg';
 
 	let message = '';
+
+	function handleSubmit() {
+		console.log('User sent:', message);
+
+		// Clear the message after sending
+		message = '';
+	}
 </script>
 
-<div
+<form
+	on:submit|preventDefault={handleSubmit}
 	class="flex w-[calc(full-4rem)] shrink-0 basis-16 flex-row-reverse flex-nowrap items-center gap-3 overflow-visible rounded-3xl p-2 outline-2 -outline-offset-2 focus-within:outline focus-within:outline-blue-200"
 >
 	<button
+		type="submit"
 		class="peer h-11 w-11 rounded-2xl bg-blue-600 transition hover:bg-blue-700 hover:shadow-md active:bg-blue-600 active:shadow-none disabled:bg-blue-200"
 		disabled={message.trim() === ''}
 	>
@@ -20,4 +29,4 @@
 		bind:value={message}
 	/>
 	<hr class="absolute bottom-16 left-4 right-4 bg-slate-200 peer-focus:hidden" />
-</div>
+</form>
