b1a83a9038c1854bcc18617142fe67c5c7239a8d
Hunter Bryant <hunter.bryant2016@gmail.com>
2023-11-19 15:57:26 -0700

    Fixed a text input focus bug

--- src/lib/TextInput.svelte
+++ src/lib/TextInput.svelte
@@ -7,7 +7,7 @@
 	class="flex w-[calc(full-4rem)] shrink-0 basis-16 flex-row-reverse flex-nowrap items-center gap-3 overflow-visible rounded-3xl p-2 outline-2 -outline-offset-2 focus-within:outline focus-within:outline-blue-300"
 >
 	<button
-		class="h-11 w-11 rounded-2xl bg-blue-600 transition hover:bg-blue-700 hover:shadow-md active:bg-blue-600 active:shadow-none"
+		class="peer h-11 w-11 rounded-2xl bg-blue-600 transition hover:bg-blue-700 hover:shadow-md active:bg-blue-600 active:shadow-none"
 	>
 		<img src={arrowup} alt="Up arrow icon" class="m-auto flex-none" />
 	</button>
