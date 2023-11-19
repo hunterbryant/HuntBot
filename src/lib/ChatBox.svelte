73c1d89f2398ea0e82a3c14cc427d72fa5140469
Hunter Bryant <hunter.bryant2016@gmail.com>
2023-11-19 16:03:52 -0700

    Refactored to move greeting message

--- src/lib/ChatBox.svelte
+++ src/lib/ChatBox.svelte
@@ -2,13 +2,14 @@
 	import TextInput from './TextInput.svelte';
 	import UserMessage from './UserMessage.svelte';
 	import BotMessage from './BotMessage.svelte';
+	import GreetingMessage from './GreetingMessage.svelte';
 </script>
 
 <div
 	class="flex-col-rev absolute bottom-2 left-2 right-2 flex max-h-[calc(100vh-1rem)] w-[calc(full-4rem)] flex-col flex-nowrap overflow-hidden rounded-3xl bg-white shadow-md sm:left-auto sm:max-h-[calc(100vh-2rem)] sm:w-[calc(27.5rem)]"
 >
 	<!-- This initial "message" acts as the header and original kickoff button -->
-	<BotMessage />
+	<GreetingMessage />
 
 	<!-- This is the scrollable zone -->
 	<div class="overflow-scroll pb-4">
--- src/lib/GreetingMessage.svelte
+++ src/lib/GreetingMessage.svelte
@@ -11,4 +11,10 @@
 		class="h-11 w-11 flex-none rounded-2xl outline outline-1 outline-slate-200"
 	/>
 	<p class="grow">Hello 👋, I’m HuntBot</p>
+	<button
+		class="h-11 rounded-2xl bg-blue-600 px-3 text-white transition hover:bg-blue-700 hover:shadow-md active:bg-blue-600 active:shadow-none"
+	>
+		Not another GPT
+	</button>
+	<hr class="absolute left-4 right-4 top-16 bg-slate-200 peer-focus:hidden" />
 </div>
