4f2f63ae10792fd5476897c789d517d88ebc39d9
Hunter Bryant <hunter.bryant2016@gmail.com>
2023-12-12 00:27:47 -0700

    Updated base grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,21 +10,52 @@
 	/>
 </svelte:head>
 
-<div
-	class="inset-x-0 mx-auto grid h-full w-full max-w-screen-2xl grid-cols-5 grid-rows-6 gap-4 px-2 py-16 sm:grid-cols-9 sm:px-16"
->
+<!-- Handle horizontal bounds -->
+<div class="inset-x-0 mx-auto flex w-full max-w-screen-2xl flex-col gap-4 px-2 py-16 sm:px-16">
 	<!-- This div covers the first vertical half of the nav bar -->
-	<div class="col-span-5 row-span-3 flex">
-		<h1 class="my-auto text-6xl font-bold tracking-tighter">
-			I’m a designer that will build you a product your users <span class="font-serif">really</span>
-			want.
-		</h1>
+	<div
+		class="grid h-[calc(calc(50vh-64px)-0.5rem)] grow grid-cols-5 grid-rows-1 gap-4 pt-4 sm:grid-cols-9"
+	>
+		<div class="col-span-5 col-start-1 row-span-1 row-start-1 flex">
+			<h1 class="my-auto text-6xl font-bold tracking-tighter">
+				I’m a designer that will build you a product your users <span class="font-serif italic"
+					>really</span
+				>
+				want.
+			</h1>
+		</div>
 	</div>
-	<div class="col-start-4 col-end-10 row-span-2 overflow-hidden rounded bg-stone-200">
-		<img
-			src={StudioImage}
-			alt="A portrait of Hunter in a studio"
-			class="m-auto block h-full w-full object-cover"
-		/>
+
+	<!-- Personal image -->
+	<div class="grid aspect-video h-[calc(50vh-11.25rem)] grow grid-cols-5 gap-4 sm:grid-cols-9">
+		<div class="col-start-4 col-end-10 overflow-hidden rounded bg-stone-200">
+			<img
+				src={StudioImage}
+				alt="A portrait of Hunter in a studio"
+				class="m-auto block h-full w-full object-cover"
+			/>
+		</div>
+	</div>
+
+	<!-- Case Study -->
+	<div class="grid aspect-video h-[calc(50vh-11.25rem)] grow grid-cols-5 gap-4 sm:grid-cols-9">
+		<div class="col-start-4 col-end-10 overflow-hidden rounded bg-stone-200">
+			<img
+				src={StudioImage}
+				alt="A portrait of Hunter in a studio"
+				class="m-auto block h-full w-full object-cover"
+			/>
+		</div>
+	</div>
+
+	<!-- Case Study -->
+	<div class="grid aspect-video h-[calc(50vh-11.25rem)] grow grid-cols-5 gap-4 sm:grid-cols-9">
+		<div class="col-start-4 col-end-10 overflow-hidden rounded bg-stone-200">
+			<img
+				src={StudioImage}
+				alt="A portrait of Hunter in a studio"
+				class="m-auto block h-full w-full object-cover"
+			/>
+		</div>
 	</div>
 </div>
