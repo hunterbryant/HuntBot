514192ae092ab86e12d33f4c6cfd999e43bd1a49
hunterbryant <hunter.bryant2016@gmail.com>
2023-12-28 13:19:32 -0700

    Responsive breakpoints for info about slice

--- src/lib/slices/InfoEducationImage/index.svelte
+++ src/lib/slices/InfoEducationImage/index.svelte
@@ -12,27 +12,29 @@
 >
 	<!-- Out of box metadata -->
 	<div
-		class="col-span-1 col-start-3 row-start-2 mt-1 flex flex-col justify-start gap-2 text-right text-xs uppercase tracking-wider text-stone-500"
+		class="col-span-1 col-start-1 row-start-3 mt-1 flex flex-col justify-start gap-2 text-left text-xs uppercase tracking-wider text-stone-500 sm:col-start-3 md:row-start-2"
 	>
 		Education
 	</div>
 
 	<!-- Text Block -->
 	<div
-		class="col-span-3 col-start-4 mb-8 flex flex-col gap-8 text-balance text-stone-900 *:row-span-1 *:flex *:flex-col"
+		class="col-span-full col-start-2 row-start-2 mb-8 flex flex-col gap-8 text-balance text-stone-900 *:row-span-1 *:flex *:flex-col sm:col-span-3 sm:col-start-4 md:row-start-1"
 	>
 		<PrismicRichText field={slice.primary.body} />
 	</div>
 
 	<!-- Education Block -->
 	<div
-		class="col-span-3 col-start-4 row-start-2 flex flex-col gap-8 text-balance text-stone-900 *:row-span-1 *:flex *:flex-col *:gap-4"
+		class="col-span-4 col-start-1 row-start-4 flex flex-col gap-8 text-balance text-stone-900 *:row-span-1 *:flex *:flex-col *:gap-4 sm:col-span-3 sm:col-start-4 sm:row-start-3 md:row-start-2"
 	>
 		<PrismicRichText field={slice.primary.education} />
 	</div>
 
 	<!-- Image Column -->
-	<div class="col-span-3 row-span-2 row-start-1 flex flex-col gap-8">
+	<div
+		class="col-span-full col-start-1 row-span-1 row-start-1 mb-12 flex flex-col gap-8 sm:col-span-3 sm:col-start-4 md:col-start-auto md:row-span-2 md:row-start-1 md:mb-0"
+	>
 		<PrismicImage field={slice.primary.image} class="h-full rounded object-cover" />
 	</div>
 </section>
