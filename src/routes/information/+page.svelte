2b9e61b408c7fe856d61e7505711728f9f70ad12
hunterbryant <hunter.bryant2016@gmail.com>
2023-12-27 18:40:57 -0700

    Added info page layout framework grid

--- src/routes/information/+page.svelte
+++ src/routes/information/+page.svelte
@@ -17,7 +17,19 @@
 	<meta property="og:image" content={data.page.data.meta_image.url} />
 </svelte:head>
 
-<div class="flex h-screen justify-center p-8">
-	<h1 class="text-xl">About</h1>
-	<SliceZone slices={data.page.data.slices} {components} />
+<!-- Handle horizontal bounds -->
+<div
+	class="inset-x-0 z-30 mx-auto flex w-full max-w-screen-xl flex-col gap-4 px-2 pb-16 pt-24 sm:px-8 sm:pb-24 sm:pt-16 lg:px-16"
+>
+	<!-- Grid -->
+	<div class="grid grow grid-cols-5 gap-4 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-9">
+		<div
+			class="col-start-1 col-end-6 flex h-full flex-col justify-end overflow-hidden rounded text-6xl font-bold tracking-tighter text-stone-800 sm:col-start-4 sm:col-end-7 sm:h-[calc(50svh-9rem)] sm:text-5xl md:col-end-8 md:text-7xl lg:col-end-10 lg:text-8xl xl:text-9xl"
+		>
+			{data.page.data.header}
+		</div>
+		<div class="col-start-1 col-end-6 flex flex-col sm:col-start-4 sm:col-end-7">
+			<SliceZone slices={data.page.data.slices} {components} />
+		</div>
+	</div>
 </div>
