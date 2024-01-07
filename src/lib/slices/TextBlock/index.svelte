654e82c0074d33db16d0292163f22b53eb2feef3
hunterbryant <hunter.bryant2016@gmail.com>
2024-01-06 22:40:13 -0700

    Edited styling of text block slice

--- src/lib/slices/TextBlock/index.svelte
+++ src/lib/slices/TextBlock/index.svelte
@@ -11,7 +11,7 @@
 <section
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
-	class="col-span-full mb-8 flex flex-col items-stretch gap-8 text-stone-800 *:text-balance sm:col-span-4 sm:col-start-4 sm:mb-0 md:mb-32 [&>h2]:text-3xl [&>h2]:font-bold [&>h2]:leading-none [&>h2]:tracking-tight sm:[&>h2]:text-2xl [&>p]:text-xl [&>p]:leading-normal"
+	class="[&>p]:text-md col-span-full mb-8 flex flex-col items-stretch gap-8 text-stone-800 *:text-balance sm:col-span-4 sm:col-start-4 sm:mb-0 md:mb-32 [&>h2]:text-2xl [&>h2]:font-bold [&>h2]:leading-none [&>h2]:tracking-tight sm:[&>h2]:text-3xl [&>p]:leading-normal sm:[&>p]:text-xl"
 >
 	<PrismicRichText field={slice.primary.content} />
 </section>
