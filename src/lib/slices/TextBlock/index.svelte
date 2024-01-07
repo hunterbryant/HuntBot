5c785b5bfdafcf7012fbf9217ad2e12ccbf7f0ac
hunterbryant <hunter.bryant2016@gmail.com>
2024-01-06 22:37:28 -0700

    Added initial text block slice

--- src/lib/slices/TextBlock/index.svelte
+++ src/lib/slices/TextBlock/index.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
+	import { PrismicRichText } from '@prismicio/svelte';
 
 	export let slice: Content.TextBlockSlice;
+
+	//Silences runtime svelte unused prop warnings
+	$$restProps;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	Placeholder component for {slice.slice_type} (variation: {slice.variation}) Slices
+<section
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+	class="col-span-full mb-8 flex flex-col items-stretch gap-8 text-stone-800 *:text-balance sm:col-span-4 sm:col-start-4 sm:mb-0 md:mb-32 [&>h2]:text-3xl [&>h2]:font-bold [&>h2]:leading-none [&>h2]:tracking-tight sm:[&>h2]:text-2xl [&>p]:text-xl [&>p]:leading-normal"
+>
+	<PrismicRichText field={slice.primary.content} />
 </section>
