7514138a2dc9f3c95b7d18c813180e853d1c49b6
Hunter Bryant <hunter.bryant2016@gmail.com>
2024-01-12 16:46:28 -0700

    Additional styling support for links and h3 headings

--- src/lib/slices/TextBlock/index.svelte
+++ src/lib/slices/TextBlock/index.svelte
@@ -11,8 +11,8 @@
 <section
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
-	class="sm:[&>p]:text-md col-span-full mb-4 flex flex-col items-stretch gap-2 text-stone-800 *:mb-4 *:text-balance sm:col-span-4 sm:col-start-4 sm:mb-8 [&>P>strong]:text-stone-800 [&>h2:not(:first-child)]:mt-3 [&>h2]:mb-4 [&>h2]:text-2xl [&>h2]:font-bold [&>h2]:leading-none [&>h2]:tracking-tight sm:[&>h2]:text-3xl
-	 [&>h6]:mb-0 [&>h6]:text-lg [&>h6]:font-bold [&>p]:text-base [&>p]:leading-normal [&>p]:text-stone-600 [&>ul]:ml-4 [&>ul]:list-outside [&>ul]:list-disc"
+	class="col-span-full mb-4 flex flex-col items-stretch gap-2 text-stone-800 *:mb-4 *:text-balance sm:col-span-4 sm:col-start-4 sm:mb-8 [&>h2:not(:first-child)]:mt-3 [&>h2]:mb-4 [&>h2]:text-2xl [&>h2]:font-bold [&>h2]:leading-none [&>h2]:tracking-tight sm:[&>h2]:text-3xl [&>h3]:mb-2 [&>h3]:text-xl [&>h3]:font-bold
+	 [&>h6]:mb-0 [&>h6]:text-lg [&>h6]:font-bold [&>p]:text-base [&>p]:leading-normal [&>p]:text-stone-600 [&>ul]:ml-4 [&>ul]:list-outside [&>ul]:list-disc [&_a:hover]:underline [&_a]:text-blue-600 [&_a]:decoration-2 [&_a]:underline-offset-2 [&_strong]:text-stone-800"
 >
 	<PrismicRichText field={slice.primary.content} />
 </section>
