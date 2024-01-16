74c5bd67d50cc82dcdf01a7ed56f887ec365f1c7
Hunter Bryant <hunter.bryant2016@gmail.com>
2024-01-16 10:13:47 -0700

    Better large screen text sizing

--- src/lib/slices/TextBlock/index.svelte
+++ src/lib/slices/TextBlock/index.svelte
@@ -11,8 +11,8 @@
 <section
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
-	class="prose prose-stone prose-headings:pb-1 hover:prose-a:underline prose-a:no-underline prose-a:text-blue-600 prose-a:decoration-2 prose-a::underline-offset-2 prose-headings:leading-none prose-headings:tracking-tight prose-headings:font-bold col-span-full mb-4 flex flex-col items-stretch gap-2 text-base text-stone-800 *:mb-4 *:mt-0 *:text-balance sm:col-span-3 sm:col-start-4
-	 sm:mb-8 md:col-end-8 [&>h2:not(:first-child)]:mt-3 [&>h2]:mb-4 sm:[&>h2]:text-3xl [&>h3]:mb-2 [&>h6]:mb-0 [&>h6]:text-lg [&>ol>li]:mb-2 [&>ol]:ml-2 [&>p]:leading-normal [&>p]:text-stone-700 [&_strong]:text-stone-800"
+	class="prose prose-stone col-span-full mb-4 flex flex-col items-stretch gap-2 text-base text-stone-800 lg:prose-lg *:mb-4 *:mt-0 *:text-balance prose-headings:pb-1 prose-headings:font-bold prose-headings:leading-none prose-headings:tracking-tight prose-a:text-blue-600 prose-a:no-underline prose-a:decoration-2 prose-a:underline-offset-2 hover:prose-a:underline sm:col-span-3
+	 sm:col-start-4 sm:mb-8 md:col-end-8 lg:col-end-9 [&>h2:not(:first-child)]:mt-3 [&>h2]:mb-4 sm:[&>h2]:text-3xl [&>h3]:mb-2 [&>h6]:mb-0 [&>h6]:text-lg [&>ol>li]:mb-2 [&>ol]:ml-2 [&>p]:leading-normal [&>p]:text-stone-700 [&_strong]:text-stone-800"
 >
 	<PrismicRichText field={slice.primary.content} />
 </section>
