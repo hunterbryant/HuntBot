20c7b06ab32c50d4bd50a1bafda6729fb3dba6e2
Hunter Bryant <hunter.bryant2016@gmail.com>
2024-01-23 15:55:30 -0700

    Formatted experience fields to be responsive

--- src/lib/slices/Experience/index.svelte
+++ src/lib/slices/Experience/index.svelte
@@ -6,7 +6,7 @@
 		ExperienceSlice,
 		ExperienceSliceDefaultItem
 	} from '../../../prismicio-types';
-	import { PrismicImage } from '@prismicio/svelte';
+	import { PrismicImage, PrismicRichText } from '@prismicio/svelte';
 
 	export let slice: Content.ExperienceSlice;
 
@@ -20,7 +20,6 @@
 				experience.affiliation
 			)
 		) {
-			console.log(experience.affiliation);
 			return experience.affiliation;
 		}
 	}
@@ -29,7 +28,7 @@
 <section
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
-	class="col-span-full mb-8 grid grow grid-cols-subgrid items-stretch gap-4 sm:mb-0 md:mb-32"
+	class="col-span-full mb-8 grid grow grid-cols-subgrid items-stretch gap-4 gap-y-4 sm:mb-0 md:mb-32"
 >
 	<!-- Out of box metadata -->
 	<div
@@ -40,26 +39,37 @@
 
 	<!-- Experience Blocks -->
 	<div
-		class="col-span-full row-start-2 grid grid-cols-subgrid gap-y-8 sm:col-start-4 sm:row-start-1 sm:gap-y-16"
+		class="col-span-full row-start-2 grid grid-cols-subgrid gap-y-12 sm:col-start-4 sm:row-start-1 sm:gap-y-16"
 	>
 		{#each slice.items as item}
-			<div class="col-span-full grid grid-cols-subgrid">
-				<div class="col-span-2 flex flex-col items-start">
-					<PrismicImage field={typeAffliation(item)?.data?.logo} height="64" />
-					<h3 class="mt-8 font-bold leading-tight">{item.title}</h3>
-					<p class="text-stone-600">
-						{typeAffliation(item)?.data?.verbose_title ?? typeAffliation(item)?.data?.title}
-					</p>
-					<p
-						class="mt-6 rounded bg-stone-200 p-1 pb-[2px] text-xs uppercase tracking-wider text-stone-900"
-					>
-						{item.timeframe}
-					</p>
-					{#if item.caption}
-						<p class="mt-1 rounded p-1 pb-[2px] text-xs uppercase tracking-wider text-stone-600">
-							{item.caption}
+			<div class="col-span-full grid grid-cols-subgrid gap-y-6">
+				<div class="col-span-5 flex flex-col items-start sm:col-span-3 lg:col-span-2">
+					<div class="flex flex-row items-center justify-stretch gap-4 lg:flex-col lg:items-start">
+						<PrismicImage field={typeAffliation(item)?.data?.logo} height="64" />
+						<div>
+							<h3 class="font-bold leading-tight">{item.title}</h3>
+							<p class="text-stone-600">
+								{typeAffliation(item)?.data?.verbose_title ?? typeAffliation(item)?.data?.title}
+							</p>
+						</div>
+					</div>
+					<div class="flex flex-col items-start">
+						<p
+							class="mt-6 rounded bg-stone-200 p-1 pb-[2px] text-xs uppercase tracking-wider text-stone-900 lg:mt-6"
+						>
+							{item.timeframe}
 						</p>
-					{/if}
+						{#if item.caption}
+							<p
+								class="mt-1 rounded p-1 pb-[2px] text-xs font-medium uppercase tracking-wider text-stone-600"
+							>
+								{item.caption}
+							</p>
+						{/if}
+					</div>
+				</div>
+				<div class="col-span-5 text-stone-600 sm:col-span-3 md:col-span-4">
+					<PrismicRichText field={item.body} />
 				</div>
 			</div>
 		{/each}
