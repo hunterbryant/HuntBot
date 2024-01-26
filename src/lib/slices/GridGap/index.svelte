3483ba82bc29f8a2cb83da772d5f8c4a6c37383e
Hunter Bryant <hunter.bryant2016@gmail.com>
2024-01-25 18:39:57 -0700

    Grid gap stylistic adjustments

--- src/lib/slices/GridGap/index.svelte
+++ src/lib/slices/GridGap/index.svelte
@@ -5,8 +5,7 @@
 </script>
 
 <div
-	field={slice.primary.project}
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
-	class="group col-span-full flex hidden cursor-pointer flex-col sm:col-span-3 md:col-span-2 md:block"
+	class="group col-span-full hidden cursor-pointer flex-col sm:col-span-3 md:col-span-2 md:block"
 ></div>
