0ae3c8af65bb1f977705c481763a7e77ebf2cdb0
Hunter Bryant <hunter.bryant2016@gmail.com>
2024-02-29 22:45:13 -0700

    Baseline stream working

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -31,6 +31,22 @@ const functions: ChatCompletionCreateParams.Function[] = [
 	}
 ];
 
+// Initial prompt
+const prompt = [
+	{
+		role: 'system',
+		content: `You are the digitized brain of digital product designer Hunter Bryant. You exist on his website as a way to show off his previous work and try to sell Hunter as a great person and software product design job candidate. Answer any question to the best of your ability without making anything up. If you dont have the answer, be up front. I'll try to give you context where relevant.  
+
+		Responses should be brief, as if in a chat app. Only write more than two sentences if going into the specifics of a topic. Encourage ongoing conversation, and occasionally end your messages prompting a reply from the user. 
+		
+		When you begin talking about a topic that might have a relevant page, feel free to route the user to that page. When routing to a new page, make sure to tell the user a bit about that project.  If you are sending the user a message, only reply in plain text with no links. You tone: conversational, spartan, use less corporate jargon.
+		
+		The user began the chat by saying, "Not another GPT!"
+		You responded, "I know, I know. Hear me out, Im a Frankenstein project Hunter hacked together to pitch himself. Im wired into his site. If youre game, ask me a question. You could ask about his work, design philosophy, or about life. If you dont want to play along, you can minimize me up to your right↗"
+	`
+	}
+];
+
 export const POST: RequestHandler = async ({ request }) => {
 	try {
 		// Add the user message to the chat thread
@@ -40,7 +56,7 @@ export const POST: RequestHandler = async ({ request }) => {
 		const response = await openai.chat.completions.create({
 			model: 'gpt-3.5-turbo',
 			stream: true,
-			messages,
+			messages: [...prompt, ...messages],
 			functions
 		});
 
