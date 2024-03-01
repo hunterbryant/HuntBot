c75e8ff3fd2a2fd6ca387f0a683c933722ce6625
Hunter Bryant <hunter.bryant2016@gmail.com>
2024-03-01 01:32:38 -0700

    Fixed env var build error

--- src/routes/api/crawl/+server.ts
+++ src/routes/api/crawl/+server.ts
@@ -1,11 +1,11 @@
-import { PINECONE_INDEX } from '$env/static/private';
-import seed from './seed';
+import { env } from '$env/dynamic/private';
 import { json, type RequestHandler } from '@sveltejs/kit';
+import seed from './seed';
 
 export const POST: RequestHandler = async ({ request }) => {
 	try {
 		const { url, options } = await request.json();
-		const documents = await seed(url, 1, PINECONE_INDEX, options);
+		const documents = await seed(url, 1, env.PINECONE_INDEX, options);
 		return json({ documents });
 	} catch (error) {
 		return json({ error: `An error occurred while processing context: ${error}` }, { status: 500 });
